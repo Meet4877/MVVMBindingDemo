bb20166e29a192ec9ba0df6dea129a2c271bd6a9
Meet Shingala <shingalameet4877@gmail.com>
2023-06-13 23:36:34 +0530

    create template

---
+++ MVVMBindingDemo/MVVMBindingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMBindingDemo
+//
+//  Created by Meet Shingala on 13/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
